c5b797cb5ce3aea4a338272f8b84a5988014ebb4
Manuel Esquivel <36414531+manuel1155@users.noreply.github.com>
2019-09-17 17:54:35 -0500

    dd
    dddddddd

--- src/app/fruta/fruta.component.ts
+++ src/app/fruta/fruta.component.ts
@@ -1,44 +1,26 @@
-import { Component, OnInit } from '@angular/core';
-
-@Component({
-  selector: 'app-fruta',
-  templateUrl: './fruta.component.html',
-  styleUrls: ['./fruta.component.scss']
-})
-export class FrutaComponent implements OnInit {
-
-  nombre:string;
-  descripcion:string;
-  activo:boolean;
-  imagen1:string;
-  imagen2:string;
-  cantidad:number;
-  otros:any;
-  url:string;
-  displayImagen:string;
-
-  constructor() {
-    
-    this.nombre="Mango";
-    this.descripcion="El mango es una fruta cítrica que crece en la Zona Intertropical y es de pulpa carnosa y dulce.";
-    this.activo=true;
-    this.imagen1='https://www.pnglot.com/pngfile/detail/180-1805316_mango-png-image-free-download-imagenes-de-mango.png';
-    this.imagen2='https://www.searchpng.com/wp-content/uploads/2019/02/Mango-transparent-PNG-1024x1024.png';
-    this.cantidad=15;
-    this.url='https://es.wikipedia.org/wiki/Mango_(fruta)';
-    this.otros=['Vitamina C','Vitamina E','Vitamina K','Calcio','Hierro','Magnesio'];
-    this.displayImagen=this.imagen1;
-   }
-
-   changeImagen(){
-     if(this.displayImagen==this.imagen1){
-      this.displayImagen=this.imagen2
-     }else{
-      this.displayImagen=this.imagen1
-     }
-   }
-
-  ngOnInit() {
-  }
-
-}
+<div class="card m-3">
+    <h5 class="card-header">Formulario Cliente</h5>
+    <div class="card-body">
+        <form>
+            <div class="form-row">
+                <div class="form-group col-5">
+                    <label>Nombres</label>
+                    <input type="text" class="form-control"  />
+                </div>
+                <div class="form-group col-5">
+                    <label>Apellidos</label>
+                        <input type="text" class="form-control"/>
+                    </div>
+                    <div class="form-group col-2">
+                        <label>Se enteró:</label>
+                        <select class="form-control">
+                            <option value=""></option>
+                            <option value="Página Web">Página Web</option>
+                            <option value="Llamada Telefónica">Llamada Telefónica</option>
+                            <option value="Visita a tienda">Visita a tienda</option>
+                        </select>
+                    </div>
+                </div>
+        </form>
+    </div>
+</div>
