0260d4c4d2603475f517dea57f1eaf2061702eb1
Manuel Esquivel <36414531+manuel1155@users.noreply.github.com>
2019-09-17 18:25:33 -0500

    pppp
    ppp

--- src/app/fruta/fruta.component.ts
+++ src/app/fruta/fruta.component.ts
@@ -1,32 +1,31 @@
-<div class="card m-3">
-    <h5 class="card-header">Formulario Cliente</h5>
-    <div class="card-body">
-        <form [formGroup]="clienteForm" (ngSubmit)="onSubmit()">
-            <div class="form-row">
-                <div class="form-group col-5">
-                    <label>Nombres</label>
-                    <input type="text" class="form-control" formControlName="nombres"  />
-                </div>
-                <div class="form-group col-5">
-                    <label>Apellidos</label>
-                        <input type="text" class="form-control" formControlName="apellidos"/>
-                    </div>
-                    <div class="form-group col-2">
-                        <label>Se enteró:</label>
-                        <select class="form-control" formControlName="entero">
-                            <option value=""></option>
-                            <option value="Página Web">Página Web</option>
-                            <option value="Llamada Telefónica">Llamada Telefónica</option>
-                            <option value="Visita a tienda">Visita a tienda</option>
-                        </select>
-                    </div>
-                </div>
-                <div class="text-center">
-                    <button class="btn btn-primary" type="submit" > Registrar</button>  
-                    <button class="btn btn-danger" 
-                    type="reset" (click)="onReset()">Cancelar</button>
-                </div>
-        </form>
-    </div>
-</div>
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, 
+	      FormGroup, 
+        Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-cliente',
+  templateUrl: './cliente.component.html',
+  styleUrls: ['./cliente.component.scss']
+})
+export class ClienteComponent implements OnInit {
+
+  clienteForm: FormGroup;
+  submitted = false;
+
+  constructor(
+    private fb:FormBuilder
+  ) { 
+    this.clienteForm = this.fb.group({
+      entero: ['', Validators.required],
+      nombres: ['', Validators.required],
+      apellidos: ['', Validators.required],     
+    });
+  }
+
+  ngOnInit() {
+  }
+
+}
+
 
