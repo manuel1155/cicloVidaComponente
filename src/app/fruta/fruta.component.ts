47bc30f79bcb6552de859ddf189092e48af6b4c0
Manuel Esquivel <36414531+manuel1155@users.noreply.github.com>
2019-09-17 18:43:18 -0500

    a
    a

--- src/app/fruta/fruta.component.ts
+++ src/app/fruta/fruta.component.ts
@@ -1,31 +1,44 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, 
-	      FormGroup, 
-        Validators } from '@angular/forms';
+<div class="card m-3">
+    <h5 class="card-header">Formulario Cliente</h5>
+    <div class="card-body">
+        <form [formGroup]="clienteForm" (ngSubmit)="onSubmit()">
+            <div class="form-row">
+                <div class="form-group col-5">
+                    <label>Nombres</label>
+                    <input type="text" class="form-control" formControlName="nombres" [ngClass]="{ 'is-invalid': submitted && f.nombres.errors }"/>
+                    <div *ngIf="submitted && f.nombres.errors" class="invalid-feedback">
+                        <div *ngIf="f.nombres.errors.required">El campo nombres es requerido</div>
+                    </div>
+                </div>
 
-@Component({
-  selector: 'app-cliente',
-  templateUrl: './cliente.component.html',
-  styleUrls: ['./cliente.component.scss']
-})
-export class ClienteComponent implements OnInit {
+                <div class="form-group col-5">
+                    <label>Apellidos</label>
+                    <input type="text" class="form-control" formControlName="apellidos" [ngClass]="{ 'is-invalid': submitted && f.apellidos.errors }"/>
+                    <div *ngIf="submitted && f.apellidos.errors" class="invalid-feedback">
+                        <div *ngIf="f.apellidos.errors.required">El campo apellidos es requerido</div>
+                    </div>
+                </div>
+                <div class="form-group col-2">
+                    <label>Se enteró:</label>
+                    <select class="form-control" formControlName="entero" [ngClass]="{ 'is-invalid': submitted && f.entero.errors }">
+                        <option value=""></option>
+                        <option value="Página Web">Página Web</option>
+                        <option value="Llamada Telefónica">Llamada Telefónica</option>
+                        <option value="Visita a tienda">Visita a tienda</option>
+                    </select>
+                    <div *ngIf="submitted && f.entero.errors" class="invalid-feedback">
+                        <div *ngIf="f.entero.errors.required">Este campo es requerido</div>
+                    </div>
+                </div>
+                </div>
+                <div class="text-center">
+                    <button class="btn btn-primary" type="submit" > Registrar</button>  
+                    <button class="btn btn-danger" 
+                    type="reset" (click)="onReset()">Cancelar</button>
+                </div>
+        </form>
+    </div>
+</div>
 
-  clienteForm: FormGroup;
-  submitted = false;
-
-  constructor(
-    private fb:FormBuilder
-  ) { 
-    this.clienteForm = this.fb.group({
-      entero: ['', Validators.required],
-      nombres: ['', Validators.required],
-      apellidos: ['', Validators.required],     
-    });
-  }
-
-  ngOnInit() {
-  }
-
-}
-
-
+<pre> {{ clienteForm.value | json }} </pre>
+<p> {{ clienteForm.valid }} </p>
